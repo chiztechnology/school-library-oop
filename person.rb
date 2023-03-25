a7d73448429dfc7e36ec590b86fd8f4ba10d00b9
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-03-26 00:39:14 +0200

    Decorate classes

--- person.rb
+++ person.rb
@@ -1,9 +1,16 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+  def initialize(age, name, parent_permission: true)
+    super()
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -18,6 +25,31 @@ class Person
   end
 
   private :of_age?
+
+  def correct_name
+    @name
+  end
 end
 
-Person.new(14, 'Israel')
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.chars.slice(0, 10).join
+  end
+end
